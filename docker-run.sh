81e4970ed7f881c415e984f680e438785237ce10
Michael Droogleever <droogmic@gmail.com>
2017-01-01 14:39:34 +0100

    update docker run script with tag

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,6 @@
-docker run -d --name=rpi-cam  -p=80:80/tcp --volume=/opt/vc:/opt/vc --device=/dev/vchiq --device=/dev/vcsm droogmic/rpi-cam-web
-
+if [ $# -eq 0 ]
+  then
+    docker run -d --name=rpi-cam  -p=80:80/tcp --volume=/opt/vc:/opt/vc --device=/dev/vchiq --device=/dev/vcsm droogmic/rpi-cam-web
+  else
+    docker run -d --name=rpi-cam  -p=80:80/tcp --volume=/opt/vc:/opt/vc --device=/dev/vchiq --device=/dev/vcsm droogmic/rpi-cam-web:$1
+fi
