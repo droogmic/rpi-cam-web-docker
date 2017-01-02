792e35b8e036a2078d54bb77d06fbaecd69fb03f
Michael Droogleever <droogmic@gmail.com>
2017-01-03 00:22:58 +0100

    remove archive

--- archive/docker-run.sh
+++
@@ -1,6 +0,0 @@
-if [ $# -eq 0 ]
-  then
-    docker run -d --name=rpi-cam  -p=80:80/tcp --volume=/opt/vc:/opt/vc --device=/dev/vchiq --device=/dev/vcsm droogmic/rpi-cam-web
-  else
-    docker run -d --name=rpi-cam  -p=80:80/tcp --volume=/opt/vc:/opt/vc --device=/dev/vchiq --device=/dev/vcsm droogmic/rpi-cam-web:$1
-fi
